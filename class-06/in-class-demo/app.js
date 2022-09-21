ea01c362c6afead0c34fb50fdbcd95fb9b2bd18a
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-09-21 15:36:08 -0700

    Cleaning up/commenting

--- class-06/in-class-demo/app.js
+++ class-06/in-class-demo/app.js
@@ -1,30 +1,38 @@
-// let message = "Hello Class!";
-// alert("What is your name?");
-// let theirNameGoesHere = prompt("What is your name?");
+// declare a variable called message, assign it the value of a string
+let message = "Hello Class!";
 
-// console.log(theirNameGoesHere);
+// alert the user, pop-up box
+alert("What is your name?");
 
-// // let usersName = prompt("What is your name?");
-// // // string concatenation
-// // console.log("Hi " +usersName + " welcome to my page!");
-// // // console.log(message);
-// // // alert(message);
-// // document.write("Hi " + usersName + " welcome to my page!");
+// declare a variable, assign it the value returned from the prompt
+// the value returned from a prompt is whatever the user types
+let theirNameGoesHere = prompt("What is your name?");
 
-// // structure of conditional if/else statement
-// // if (this is true){ execute this code } 
+// print the value of the variable to the console
+console.log(theirNameGoesHere);
 
-// if (theirNameGoesHere == "Kassie"){
-//   alert("Hiya Teach!");
-// } else if (theirNameGoesHere == "Ben") {
-//   alert("TAs are rockstars!");
-// } else if (theirNameGoesHere == "Aaron") {
-//   alert("TAs are rockstars!");
-// } else if (theirNameGoesHere == "Joey"){
-//   alert("TAs are rockstars!");
-// } else { 
-//   alert("Welcome to Class 6, javascript is magical!");
-// }
+// declare a variable called usersName, assign it the return from the prompt
+let usersName = prompt("What is your name?");
+
+// string concatenation -- smooshing strings and values together into a big string
+console.log("Hi " +usersName + " welcome to my page!"); // console.log() prints to the console only
+
+document.write("Hi " + usersName + " welcome to my page!"); // document.write() prints to the actual HTML page
+
+// structure of conditional if/else statement
+// if (this is true){execute this code} else {execute this code}
+
+if (theirNameGoesHere == "Kassie"){
+  alert("Hiya Teach!");
+} else if (theirNameGoesHere == "Ben") {
+  alert("TAs are rockstars!");
+} else if (theirNameGoesHere == "Aaron") {
+  alert("TAs are rockstars!");
+} else if (theirNameGoesHere == "Joey"){
+  alert("TAs are rockstars!");
+} else { 
+  alert("Welcome to Class 6, Javascript is magical!");
+}
 
 let number = 42;
 
@@ -44,6 +52,3 @@ document.write("Hi");
 document.write("Hi");
 document.write("Hi");
 document.write("Hi");
-
-
-
