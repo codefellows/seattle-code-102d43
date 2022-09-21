10995ed04c1fd2f01fca389c0d1a591c2f1a2d1e
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-09-21 15:30:31 -0700

    Uploading class 6 demo

---
+++ class-06/in-class-demo/app.js
@@ -0,0 +1,49 @@
+// let message = "Hello Class!";
+// alert("What is your name?");
+// let theirNameGoesHere = prompt("What is your name?");
+
+// console.log(theirNameGoesHere);
+
+// // let usersName = prompt("What is your name?");
+// // // string concatenation
+// // console.log("Hi " +usersName + " welcome to my page!");
+// // // console.log(message);
+// // // alert(message);
+// // document.write("Hi " + usersName + " welcome to my page!");
+
+// // structure of conditional if/else statement
+// // if (this is true){ execute this code } 
+
+// if (theirNameGoesHere == "Kassie"){
+//   alert("Hiya Teach!");
+// } else if (theirNameGoesHere == "Ben") {
+//   alert("TAs are rockstars!");
+// } else if (theirNameGoesHere == "Aaron") {
+//   alert("TAs are rockstars!");
+// } else if (theirNameGoesHere == "Joey"){
+//   alert("TAs are rockstars!");
+// } else { 
+//   alert("Welcome to Class 6, javascript is magical!");
+// }
+
+let number = 42;
+
+let usersGuess = prompt("Guess what number I'm thinking of between 1 & 50");
+if (usersGuess == number){
+  alert("That's correct!");
+} else if (usersGuess > number){
+  alert("That's too high!");
+} else if (usersGuess < number){
+  alert("That's too low!");
+} else {
+  alert("That's incorrect");
+}
+
+document.write("SHOWING WHERE THE SCRIPT IS RUNNING");
+document.write("Hi");
+document.write("Hi");
+document.write("Hi");
+document.write("Hi");
+
+
+
